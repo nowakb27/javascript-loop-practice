f6626a7ec003d669ff746ae0da249a7354868a35
nowakb27 <71862095+nowakb27@users.noreply.github.com>
2020-11-29 21:37:52 -0800

    initial commit

--- images.js
+++ images.js
@@ -15,40 +15,25 @@ $(document).ready(function () {
         $("#do-while-num").text(num - 1);
         $("#for-num").text(num);
 
+        var iterations = 0
+        var numPlus = num + 1
+        while(iterations !== numPlus) {
+            addImageToDiv("#while-loop");
+            iterations++
+        }
 
-        /*
-         Create a while loop that runs "num + 1" times.
+        var doIterations = 0
+        var numMinus = num - 1
+        do{
+            addImageToDiv("#do-while-loop");
+            doIterations++
+        }
+        while(doIterations !== numMinus)
 
-         The body of the loop should contain the statement:
-         addImageToDiv("#while-loop");
-        */
-
-
-
-
-
-        /*
-         Create a do-while loop that runs "num - 1" times.
-
-         The body of the loop should contain the statement:
-         addImageToDiv("#do-while-loop");
-        */
-
-
-
-
-
-        /*
-         Create a for loop that runs exactly "num" times.
-
-         The body of the loop should contain the statement:
-         addImageToDiv("#for-loop");
-        */
-
-
-
-
-
+        var i
+        for(i = 0; i < num; i++){
+            addImageToDiv("#for-loop");
+        }
     }
 
     function addImageToDiv(divId) {
--- text.js
+++ text.js
@@ -24,71 +24,50 @@ $(document).ready(function () {
         submitHandler: runLoops
     });
 });
-
 function runLoops() {
-    // This variable will contain one of three strings:
-    // "for-loop", "while-loop", or "do-while-loop"
     var selectedLoop = $("input[name=loop-type]:checked").val();
-
-    /*
-        Depending on which loop was selected,
-        call the appropriate function.
-
-        Possible values of selectedLoop and
-        the corresponding function calls are:
-
-        "for-loop"	    ==>  runForLoop()
-        "while-loop"    ==>  runWhileLoop()
-        "do-while-loop" ==>  runDoWhileLoop()
-    */
-
-
-
-
-
+    switch(selectedLoop){
+        case "while-loop":
+            runWhileLoop()
+            break;
+        case "do-while-loop":
+            runDoWhileLoop()
+            break;
+        default:
+            runForLoop()
+            break;
+    }
 }
-
-
 function runWhileLoop() {
     var number = parseInt($("#myNumber").val());
     var output = "";
 
-    /*
-        Use a while loop to concatenate the letter A
-        "number" times to the string "output". For example,
-        if "number" is 5, then output should be "AAAAA".
-
-        (Or for a challenge, concatenate the first "number" letters of the alphabet!)
-    */
-
-
+    var iterations = 0
+    while(iterations !== number){
+        output = output + String.fromCharCode(iterations+65)
+        iterations++
+    }
     $("#while-result").text(output);
 }
-
-
 function runForLoop() {
     var number = parseInt($("#myNumber").val());
     var sum = 0;
 
-    /*
-        Use a for loop to add the numbers 1 through "number"
-        into the variable "sum".
-    */
+    var i
+    for(i = 1; i<=number; i++){
+        sum+=i
+    }
 
     $("#for-result").text(sum);
 }
-
-
 function runDoWhileLoop() {
     var number = parseInt($("#myNumber").val());
     var output = "";
 
-    /*
-        Use a do-while loop to concatenate the numbers 1 through
-        "number" to the string "output". For example, if "number"
-        is 5, then output should be "12345".
-    */
-
-
-    $("#do-while-result").text(output);
+    var iterations = 1
+    do{
+        output = output + iterations
+        iterations++
+    }
+    while(iterations <= number)
 }
